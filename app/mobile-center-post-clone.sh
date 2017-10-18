0406bebb55c9581bd84bc260f528b97af0586322
Matthias Wenz <matwen@microsoft.com>
2017-10-18 14:08:58 -0700

    Remove build scripts

--- app/mobile-center-post-clone.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-echo "1. Post clone"
-
-
-echo "\n\n\n\n\n\nTHIS IS AWESOME!\n\n\n\n\n\n"
-
-echo "ENVIRONMENT"
-env
--- app/mobile-center-pre-build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo "2. Pre Build"
-env
-
-echo "Sleeping 30min"
-sleep 1800
