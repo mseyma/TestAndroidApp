250c568d21eeea1868137912f0fa635cc891314c
Matthias Wenz <matwen@microsoft.com>
2018-04-06 16:00:20 +0900

    Break the build 😱

--- app/src/main/java/de/bowstreet/testandroidapp/MainActivity.java
+++ app/src/main/java/de/bowstreet/testandroidapp/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MobileCenter.start(getApplication(), "675da273-5716-4855-9dd0-431fe51ebfef", Analytics.class, Crashes.class);
+        MobileCenter .s tart(getApplication(), "675da273-5716-4855-9dd0-431fe51ebfef", Analytics.class, Crashes.class);
 
 
         mButton = (Button) findViewById(R.id.button);
